357776a5254eff89c494b996750994d385b7b61c
vi_ser <vinceulisse@gmail.com>
2024-02-28 12:37:30 +0100

    bonus completato (vite ti sto amando)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script>
-import AppTitle from './components/AppTitle.vue';
+import AppJumbotron from './components/AppJumbotron.vue';
 
 export default {
   components: {
-    AppTitle,
+    AppJumbotron
   },
 }
 </script>
 
 <template>
-  <AppTitle></AppTitle>
+  <AppJumbotron></AppJumbotron>
 </template>
 
 <style></style>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,44 @@
+<script>
+
+import AppTitle from './AppTitle.vue';
+
+export default {
+    name: 'AppJumbotron',
+    components: { AppTitle }
+}
+
+</script>
+
+<template>
+    <div id="jumbotron">
+        <div class="text">
+            <AppTitle></AppTitle>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas minus cum, alias ipsa, impedit ducimus,
+                repellendus ipsum nostrum dolorem aut ad quod velit perferendis nam qui aliquam voluptatibus aperiam ea?</p>
+        </div>
+        <div class="img">
+            <img src="https://picsum.photos/300/300" alt="">
+        </div>
+    </div>
+</template>
+
+<style>
+#jumbotron {
+    display: flex;
+    gap: 100px;
+}
+
+#jumbotron .text {
+    text-align: start;
+}
+
+#jumbotron img {
+    border-radius: 20px;
+}
+
+@media screen and (max-width: 768px) {
+    #jumbotron {
+        flex-direction: column;
+    }
+}
+</style>
